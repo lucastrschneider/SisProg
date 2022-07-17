2c6c067b0604ad95aed30ee5bd96c12a209e91ba
lucastrschneider <lucastrschneider@gmail.com>
2022-07-17 15:15:11 -0300

    ♻️ Change memory to address words of 32 bits

--- prog_sys/memory.py
+++ prog_sys/memory.py
@@ -1,25 +1,21 @@
 from .singleton import Singleton
 import numpy as np
 
-MEMORY_SIZE = 2**10
+MEMORY_SIZE = 2**12
 
 class Memory(metaclass=Singleton):
     def __init__(self) -> None:
-        self.array = np.zeros(MEMORY_SIZE, dtype=np.uint8)
+        self.array = np.zeros(MEMORY_SIZE, dtype=np.uint32)
 
-
-    def __getitem__(self, index):
+    def __getitem__(self, index) -> np.uint32:
         return self.array[index]
 
-
-    def __setitem__(self, index, value):
+    def __setitem__(self, index, value: np.uint32):
         self.array[index] = value
-
 
     def reset(self):
         self.array *= 0
 
-
     def display(self):
-        np.savetxt("image.txt", self.array, fmt='%02X')
+        np.savetxt("image.txt", self.array, fmt='%08X')
 
