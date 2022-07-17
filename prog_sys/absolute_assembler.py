5103f906127270c9074679bfc9bfcbb6a524d059
Bruno Mariz <bruno.mariz@semantix.inc>
2022-07-17 20:38:45 -0300

    :pencil2: Correct method name

--- prog_sys/absolute_assembler.py
+++ prog_sys/absolute_assembler.py
@@ -2,14 +2,25 @@ from .event import Event, EventType
 from .event_motor import EventMotor
 from .memory import Memory
 
+
 class AbsoluteAssembler(EventMotor):
     def __init__(self):
         super().__init__()
 
+        # Key: Mnemonic
+        # Value: Code
+        self.symbol_table = {}
+
         self._memory = Memory()
-        self._events_reactions[EventType.ABSOLUTE_ASSEMBLER_FIRST_STEP] = self._first_step
-        self._events_reactions[EventType.ABSOLUTE_ASSEMBLER_SECOND_STEP] = self._second_step
-        self._events_reactions[EventType.ABSOLUTE_ASSEMBLER_ASSEMBLE_LINE] = self._assemble_line
+        self._events_reactions[
+            EventType.ABSOLUTE_ASSEMBLER_FIRST_STEP
+        ] = self._first_step
+        self._events_reactions[
+            EventType.ABSOLUTE_ASSEMBLER_SECOND_STEP
+        ] = self._second_step
+        self._events_reactions[
+            EventType.ABSOLUTE_ASSEMBLER_ASSEMBLE_LINE
+        ] = self._assemble_line
         self._events_reactions[EventType.ABSOLUTE_ASSEMBLER_END] = self._end
 
     def _first_step(self, event: Event):
@@ -21,5 +32,5 @@ class AbsoluteAssembler(EventMotor):
     def _assemble_line(self, event: Event):
         pass
 
-    def end(self, event: Event):
+    def _end(self, event: Event):
         pass
