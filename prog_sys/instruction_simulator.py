cdcf637c25959009912edf0c61caa7b399b9f68c
AntonioSeixas1 <78570746+AntonioSeixas1@users.noreply.github.com>
2022-07-18 04:10:42 -0300

    🐛 Change array index reference

--- prog_sys/instruction_simulator.py
+++ prog_sys/instruction_simulator.py
@@ -13,45 +13,52 @@ class InstructionSimulator(EventMotor):
         self._reg_file.reset()
         self._program_counter =  program_index
         self._instruction = '00000000000000000000000000000000'
-        self._events_reactions[EventType.FETCH_DECODE_EXECUTE_STEP] = self._first_fetch_decode_execute_step_reaction
-        self._events_reactions[EventType.FETCH_DECODE_EXECUTE_CONTINUOSLY] = self._first_fetch_decode_execute_continuosly_reaction
+        self._events_reactions[EventType.FETCH_DECODE_EXECUTE_STEP] = self._fetch_decode_execute_step_reaction
+        self._events_reactions[EventType.FETCH_DECODE_EXECUTE_CONTINUOSLY] = self._fetch_decode_execute_continuosly_reaction
         self._events_reactions[EventType.FINISH] = self._finish_reaction
 
 
-    def _first_fetch_decode_execute_reaction_step(self):
+    def _fetch_decode_execute_step_reaction(self, event: Event):
         self.fetch_decode_execute()
 
 
-    def _first_fetch_decode_execute_reaction_continuosly(self):
+    def _fetch_decode_execute_continuosly_reaction(self, event: Event):
         self.fetch_decode_execute()
-        is_to_stop = self._instruction == '11111111111111111111111111111111'
+        is_to_stop = self._instruction[0:11] == '11111111111' # stop condition
         if is_to_stop:
-            self.add_event(Event(EventType.FINISH))
+            self.add_event(Event(EventType.FINISH, ""))
         else:
-            self.add_event(Event(EventType.FETCH_DECODE_EXECUTE_CONTINUOSLY))
+            self.add_event(Event(EventType.FETCH_DECODE_EXECUTE_CONTINUOSLY, ""))
     
-    def _finish_reaction(self):
+    def _finish_reaction(self, event: Event):
+        print("end execution")
         self.reset()
 
 
     def fetch_decode_execute(self):
         self._instruction = f'{self._memory.__getitem__(self._program_counter):032b}'
         self._program_counter += 1 
+        print(self._instruction)
+        print(self._program_counter)
+        print(self._instruction[0:11])
 
 
-        if self._instruction[31:21] == '11111000010': # LDUR
-            address = int(self._instruction[20:12], base=2)
-            rn_value = self._reg_file.__getitem__(int(self._instruction[9:5], base=2))
-            rt_address = int(self._instruction[4:0], base = 2)
+        if self._instruction[0:11] == "11111000010": # LDUR
+            print("load started")
+            address = int(self._instruction[11:20], base=2)
+            print("address :", self._instruction[11:20] )
+            rn_value = self._reg_file.__getitem__(int(self._instruction[21:27], base=2))
+            rt_address = int(self._instruction[27:32], base = 2)
             rt_value = self._memory.__getitem__(rn_value+address)
             self._reg_file.__setitem__(rt_address, rt_value)   
 
-        elif self._instruction[31:21] == '11111000000': #STUR
-            address = int(self._instruction[20:12], base=2)
-            rn_value = self._reg_file.__getitem__(int(self._instruction[9:5], base=2))
-            rt_address = int(self._instruction[4:0], base = 2)
+        elif self._instruction[0:11] == '11111000000': #STUR
+            print("store started")
+            address = int(self._instruction[11:20], base=2)
+            rn_value = self._reg_file.__getitem__(int(self._instruction[21:27], base=2))
+            rt_address = int(self._instruction[27:32], base = 2)
             rt_value = self._reg_file.__getitem__(rt_address)
-            self._memmory.__setitem__(address+rn_value, rt_value)
+            self._memory.__setitem__(address+rn_value, rt_value)
         
         elif self._instruction[31:24] == '10110100': #CBZ 
             address = int(self._instruction[23:5], base = 2)
@@ -69,10 +76,11 @@ class InstructionSimulator(EventMotor):
             address = int(self._instruction[25:0], base = 2)
             self._program_counter += address
         
-        elif self._instruction[31:21] == '10001011000': #ADD
-            rn_value = self._reg_file.__getitem__(int(self._instruction[9:5], base = 2))
-            rm_value = self._reg_file.__getitem__(int(self._instruction[20:16], base = 2))
-            rd_address = self._instruction[4:0]
+        elif self._instruction[0:11] == '10001011000': #ADD
+            print("add started")
+            rn_value = self._reg_file.__getitem__(int(self._instruction[22:27], base = 2))
+            rm_value = self._reg_file.__getitem__(int(self._instruction[11:16], base = 2))
+            rd_address = int(self._instruction[27:32], base = 2)
             self._reg_file.__setitem__(rd_address, rn_value + rm_value)
 
 
