e345cfa59ec44e98bfd71ed35987bad1959e6006
Bruno Mariz <bruno.mariz@semantix.inc>
2022-07-17 22:00:26 -0300

    Add mnemonic table and first regex patterns

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from prog_sys.event_controller import EventController
+from prog_sys.absolute_assembler import AbsoluteAssembler
+from prog_sys.event import Event, EventType
+from prog_sys.memory import Memory
 from prog_sys.loader import Loader
 from prog_sys.dumper import Dumper
 from prog_sys.gui import ProgSysGUI
@@ -8,6 +11,14 @@ if __name__ == "__main__":
 
     event_controller.register_motor(Loader())
     event_controller.register_motor(Dumper())
+    memory = Memory()
+    loader = Loader()
+    dumper = Dumper()
+    absolute_assembler = AbsoluteAssembler()
+
+    loader.activate()
+    dumper.activate()
+    absolute_assembler.activate()
 
     gui = ProgSysGUI()
 
@@ -15,3 +26,15 @@ if __name__ == "__main__":
         gui.start()
     except KeyboardInterrupt:
         gui.stop()
+    dump_event = Event(
+        EventType.DUMPER_LOAD_DATA,
+        {"file": "home/test2.bin", "start_address": 15, "size": 12},
+    )
+
+    status = dumper.add_event(dump_event)
+    status = dumper.run()
+    status = dumper.run()
+
+    assemble_event = Event(EventType.ABSOLUTE_ASSEMBLER_FIRST_PASS, "home/test.asm")
+    status = absolute_assembler.add_event(assemble_event)
+    status = absolute_assembler.run()
